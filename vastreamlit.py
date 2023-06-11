7739556561329d1d9b2870a7edae8764adead8d5
Sudharshan Vijay SK <98068200+Spark1810@users.noreply.github.com>
2023-06-11 21:57:54 +0530

    Update vastreamlit.py

--- vastreamlit.py
+++ vastreamlit.py
@@ -4,10 +4,8 @@ import pyttsx3;
 import time;
 import mysql.connector;
 import speech_recognition as sr;
-import os 
-import pprint
-from pprint import pprint
-from gtts import gTTS 
+import os
+from gtts import gTTS
 import pyttsx3;
 from googletrans import Translator, constants
 from pydub import AudioSegment
@@ -18,6 +16,7 @@ import wikipedia
 import requests
 import pywintypes
 from bs4 import BeautifulSoup
+import screen_brightness_control as sbc
 import win32gui, win32con
 import os
 import math
@@ -28,6 +27,7 @@ import pyautogui
 import pywhatkit
 import datetime
 
+z="sir"
 #========================================== Voice funtion
 
 def voice(txt):
@@ -43,6 +43,75 @@ def voice(txt):
     engine.setProperty('volume', 5.0)
     engine.say(txt);
     engine.runAndWait() ;
+
+
+
+def whether():
+    st.write("Say the City Name...")
+    city=""
+        
+    url = "https://www.google.com/search?q="+"weather"+city
+    html = requests.get(url).content
+    # getting raw data
+    soup = BeautifulSoup(html, 'html.parser')
+    temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+    str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+    
+    # formatting data
+    data = str.split('\n')
+    time = data[0]
+    sky = data[1]
+    
+    # getting all div tag
+    listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+    strd = listdiv[5].text
+    
+    # getting other required data
+    pos = strd.find('Wind')
+    other_data = strd[pos:]
+    
+    # st.writeing all data
+    st.write("Temperature is", temp)
+    st.write("Time: ", time)
+    st.write("Sky Description: ", sky)
+    st.write(other_data)
+    voice("Temperature is"+temp+" \n Sky Description: "+sky)
+
+def decv():
+    try:
+        from ctypes import cast, POINTER
+        from comtypes import CLSCTX_ALL
+        from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+        import math
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        currentVolumeDb = volume.GetMasterVolumeLevel()
+        volume.GetMasterVolumeLevel()
+        volume.SetMasterVolumeLevel(currentVolumeDb - 10.0, None)
+        volume.GetMasterVolumeLevel()
+
+    except:
+        st.write("Minimum Volume Attained "+z)
+        voice("Minimum volume attained "+z)
+
+def incv():
+    try:
+        from ctypes import cast, POINTER
+        from comtypes import CLSCTX_ALL
+        from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+        import math
+        devices = AudioUtilities.GetSpeakers()
+        interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+        volume = cast(interface, POINTER(IAudioEndpointVolume))
+        currentVolumeDb = volume.GetMasterVolumeLevel()
+        volume.GetMasterVolumeLevel()
+        volume.SetMasterVolumeLevel(currentVolumeDb + 10.0, None)
+        volume.GetMasterVolumeLevel()
+
+    except:
+        st.write("Maximum Volume Attained "+z)
+        voice("Maximum volume attained "+z)
 
 def response(text):
     # Method to self shut down system
@@ -86,18 +155,13 @@ def response(text):
         voice("According to wikipedia")
         voice(result)
 
-    elif "close" in query and"tata" in query:
-        voice("Bye. Have a great day"+z)
-        st.write("Shut down initiating...")
-        Maam = Gfg()
-        Maam.quitSelf()
+    elif "close" in query and "tata" in query:
+        st.write("Closing the program!!!")
+        time.sleep(2)
+        voice("Bye. Have a great day")
+        pyautogui.hotkey('ctrl', 'w')
         
-    elif "restart system" in query:
-        voice("Bye. Have a great day"+z)
-        st.write("Restart initiating...")
-        Maam = Gfg2()
-        Maam.quitSelf()
-
+        
     elif "message" in query:
             voice("Please Enter The Message To Be Sent Sir")
             message=st.text_input("Please Enter The Message To Be Sent :")
@@ -114,10 +178,127 @@ def response(text):
                 # syntax: phone number with country code, message, hour and minutes
                 pywhatkit.sendwhatmsg('+91'+cont, message, hours, minutes)
 
+    elif "who are you" in query:
+        voice("Hello sir im TRIS your virtual assistant ! How can i help you today sir?")
+
+    elif "climate" in query:
+        whether()
+
+    elif "open" in query:
+            ap=query[4:]
+            voice("opening "+ap)
+            pyautogui.press("win")
+            time.sleep(1)
+            for i in ap:
+                pyautogui.press(i)
+            pyautogui.press('enter')
+
+    elif "increase brightness" in query:
+            for bri in sbc.get_brightness():
+                if bri>90:
+                    sbc.set_brightness(100)
+                else:
+                    sbc.set_brightness(bri+10)
+
+    elif "decrease brightness" in query:
+            for bri in sbc.get_brightness():
+                if bri<10:
+                    sbc.set_brightness(0)
+                else:
+                    sbc.set_brightness(bri-10)
+
+    elif "increase volume" in query:
+            incv()
+            voice("Increased volume")
+
+    elif "decrease volume" in query:
+            decv()
+            voice("Decreased volume")
+
+    elif "fortune time" in query:
+            st.write("Fortune Teller!!!")
+            st.write("Say the Event...")
+            voice("Say The Event"+z)
+            a=st.text_input("Enter the Event :")
+            if a:
+                import random
+                s=random.randint(1,8)
+                if s==1:
+                    st.write(a," will not happen in this lifetime!!!")
+                    voice(a+" will not happen in this lifetime!!!")
+                elif s==2:
+                    st.write(a," will happen surely!!!")
+                    voice(a+" will happen surely!!!")
+                elif s==3:
+                    st.write(a," will happen when you are 80 years!!!")
+                    voice(a+" will happen when you are 80 years!!!")
+                elif s==4:
+                    st.write(a," may or may not happen!!!")
+                    voice(a+" may or may not happen!!!")
+                elif s==5:
+                    st.write(a," will happen when you lose something which you like the most!!!")
+                    voice(a+" will happen when you lose something which you like the most!!!")
+                elif s==6:
+                    st.write(a," can happen under gods grace!!!")
+                    voice(a+" can happen under gods grace!!!")
+                elif s==7:
+                    st.write(a," will happen after a lot of struggle!!!")
+                    voice(a+" will happen after a lot of struggle!!!")
+                elif s==8:
+                    st.write(a," will happen soon!!!")
+                    voice(a+" will happen soon!!!")
+                else:
+                    st.write(a," will not happen!!!")
+                    voice(a+" will not happen!!!")
+            
+            else:
+                st.write("Please enter an event to check")
+            time.sleep(3)
+            st.write("Note: Future changes every second and this may or may not happen")
+
+    elif "screenshot" in query:
+            import pyscreenshot
+            image = pyscreenshot.grab()
+            image.show()
+
+    elif "maximize" in query:
+            Minimize = win32gui.GetForegroundWindow()
+            win32gui.ShowWindow(Minimize, win32con.SW_MAXIMIZE)
+
+    elif "minimise" in query:
+            Minimize = win32gui.GetForegroundWindow()
+            win32gui.ShowWindow(Minimize, win32con.SW_MINIMIZE)
+
+    elif query !="":
+        import os
+        import openai
+        openai.api_key = "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"
+        def ask_chatgpt(prompt):
+            model_engine = "text-davinci-003"
+            response = openai.Completion.create(
+                engine=model_engine,
+                prompt=prompt,
+                max_tokens=1024,
+                n=1,
+                stop=None,
+                temperature=0.7,
+                top_p=1,
+                frequency_penalty=0,
+                presence_penalty=0
+                ) 
+            message = response.choices[0].text.strip()
+            return message
+        prompt =query
+        response = ask_chatgpt(prompt)
+        st.write("Tris :"+" "+response+" "+z)
+        voice(""+" "+response+" "+z)
+        
+    
+
 #----------------------------------------------------------------------
 
 st.header('VIRTUAL ASSISTANT')
-voice("Hello sir im TRIS your virtual assistant ! How can i help you today sir?")
+
 
 
 
